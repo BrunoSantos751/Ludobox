4c60418b1ff3390f0f660a89740de29d8d4dcbb0
ephremmatheus <ephrem08matheus@gmail.com>
2025-06-04 12:34:11 -0300

    navbar responsiva

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,28 +1,56 @@
+import { useState } from "react"
 import logo from "../../assets/images/logo-ludobox.png";
 import steam from "../../assets/images/steam-entrar.png";
 import './Navbar.css';
 
 export default function Navbar({ onEntrarClick }) {
+  const [click, setClick] = useState(false);
+    
+
+
+    function handleClick(){
+        setClick(!click);
+    }
+
+    function closeMobileMenu(){
+        setClick(false);
+    }
+    
   return (
     <div className="navbar-container">
       <div className="navbar-logo">
-        <a href="/"><img src={logo} alt="Logo Ludobox" /></a>
+        <a href="/" onClick={closeMobileMenu}><img src={logo} alt="Logo Ludobox" /></a>
       </div>
+
+      {/* MENU PARA DISPOSITIVOS MENORES QUE 960px */}
       <div className="navbar-links">
-        <ul>
-          <li><a href="#">Início</a></li>
-          <li><a href="#">Catálogo</a></li>
-          <li><a href="#">Tendências</a></li>
-          <li className="links login-steam">
-            <a href="#">
+        <ul className={click? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <a href="/" onClick={closeMobileMenu}>Início</a>
+            </li>
+
+          <li className="nav-item"><a href="/catalogo" onClick={closeMobileMenu}>Catálogo</a></li>
+
+          <li className="nav-item"><a href="/tendencias" onClick={closeMobileMenu}>Tendências</a></li>
+
+          <li className="nav-item login-steam">
+            <a href="#" className="nav-links-mobile login-steam"> {/*{onClick={entra com a steam}*/ }
               <img src={steam} alt="Steam" />
               Entrar com a Steam
             </a>
           </li>
-          <li className="links login-button">
-            <button onClick={onEntrarClick}>Entrar</button>
+          <li className="nav-item" >
+            <button className="nav-links-mobile login-button" onClick={onEntrarClick}>Entrar</button>
           </li>
         </ul>
+        
+
+      </div>
+
+      <div className="menu-icon" onClick={handleClick}>
+          <span className={"material-symbols-outlined"} >
+              {click? "close": "menu"}
+          </span>
       </div>
     </div>
   );
